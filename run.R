146f5e3920edb1390253e034c8832d1f630943c2
Markus Kainu <markus.kainu@fao.org>
2015-06-22 13:29:30 +0200

    initial commit

---
+++ run.R
@@ -0,0 +1,30 @@
+#################################################################################################
+# This is the main script used to control the production of FAO statistical pocketbook workflow #
+#
+################################################################################################
+
+# libraries
+
+
+
+# load the sybdata
+
+
+
+# sanitise the data
+
+
+
+
+
+
+
+## Chapters to include
+
+part1 <- FALSE
+part2 <- FALSE
+part3 <- FALSE
+part4 <- FALSE
+
+country_profiles <- FALSE
+definitions <- FALSE
