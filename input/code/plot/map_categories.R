2e54ebecd2f478c1a0599601569594b196a5d3d2
Markus Kainu <markus.kainu@fao.org>
2015-07-13 13:17:57 +0200

    mostly on maps, uploading gadget works too!
    1. map script now in syb_main.Rnw
    2. map chunks options also defined there
    3. breaks function implemented with classInt
    
    missing "manual breaks!"

--- input/code/plot/map_categories.R
+++ input/code/plot/map_categories.R
@@ -1,26 +1,33 @@
-# Creating a custom function for creating the breaks and makeing them look neat
-categories <- function(x, cat=5) {
-
+categories <- function(x, n=5,method="jenks") {
+  
   library(stringr)
-  levs <- as.data.frame(as.character(levels(cut_number(x, cat))))
+  library(classInt)
+  levs <- as.data.frame(levels(cut(x, 
+                                   breaks=data.frame(classIntervals(x,n=n,method=method)[2])[,1],
+                                   include.lowest=T,
+                                   dig.lab=10)))
   names(levs) <- "orig"
   levs$mod <- str_replace_all(levs$orig, "\\[", "")
   levs$mod <- str_replace_all(levs$mod, "\\]", "")
   levs$mod <- str_replace_all(levs$mod, "\\(", "")
   levs$lower <- gsub(",.*$","", levs$mod)
   levs$upper <- gsub(".*,","", levs$mod)
-
+  
   levs$lower <- factor(levs$lower)
   levs$lower <- round(as.numeric(levels(levs$lower))[levs$lower],0)
-
+  
   levs$upper <- factor(levs$upper)
   levs$upper <- round(as.numeric(levels(levs$upper))[levs$upper],0)
-
-  levs$labs <- paste(levs$lower,levs$upper, sep=" - ")
-
+  
+  levs$labs <- paste(levs$lower,levs$upper, sep=" ~< ")
+  
   labs <- as.character(c(levs$labs))
-  y <- cut_number(x, cat, right = FALSE, labels = labs)
+  y <- cut(x, breaks = data.frame(classIntervals(x,n=n,method=method)[2])[,1],
+                                   include.lowest=T,
+                                   dig.lab=10, labels = labs)
   y <- as.character(y)
-  y[is.na(y)] <- "No Data"
-  y <- factor(y, levels=c("No Data",labs[1:cat]))
+  #if (is.na(y)) {
+    y[is.na(y)] <- "No Data"
+    y <- factor(y, levels=c("No Data",labs[1:n]))
+  #} else y <- factor(y, levels=c("No Data",labs[1:n]))
 }
