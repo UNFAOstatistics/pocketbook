6fffa0ee825359bc08e0224ac7c38192feb595ff
Markus Kainu <markus.kainu@fao.org>
2015-09-21 17:00:55 +0200

    grid colors and font sizes as in global book

--- input/code/plot/create_map_here.R
+++ input/code/plot/create_map_here.R
@@ -34,7 +34,8 @@ create_map_here <- function(manualPalette=FALSE,manual_palette=c("#a6611a","#dfc
   p <- ggplot(data=map.plot, aes(long,lat,group=group))
   #  ---- grid below the countries ------------------------
   p <- p + geom_path(data = gr_rob, aes(long, lat, group = group, fill = NULL), 
-                     linetype = "solid", color = col.main2, alpha=.1)
+                     # linetype = "solid", color = col.main2, alpha=.1)
+                     linetype = "solid", color = "grey80", size = 0.5)
   # #  ---- whole region in grey  ------------------------
   # p <- p + geom_polygon(data=map.df[which(map.df[[region_to_report]]),],
   #                       fill = "grey70", colour = NA)
@@ -43,12 +44,12 @@ create_map_here <- function(manualPalette=FALSE,manual_palette=c("#a6611a","#dfc
   p <- p + geom_polygon(fill=NA,colour = alpha("white", 1/2), size=.4, guide = FALSE)
   p <- p + theme(legend.position = c(0.05,0.05), 
                  legend.justification=c(0,0),
-                 legend.key.size=unit(6,'mm'),
+                 legend.key.size=unit(7,'mm'),
                  legend.direction = "vertical",
                  legend.background=element_rect(colour=NA, fill=alpha("white", 1/3)),
                  #legend.background=element_rect(colour=NA, fill=NA),
-                 legend.text=element_text(size=12), 
-                 legend.title=element_text(size=12), 
+                 legend.text=element_text(size=14), 
+                 legend.title=element_text(size=14), 
                  title=element_text(size=16), 
                  panel.background = element_blank(), 
                  plot.background = element_blank(),
