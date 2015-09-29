e331a3e2d146b7e27eb6989b4f570d4410d5ea58
Markus Kainu <markus.kainu@fao.org>
2015-09-29 10:00:36 +0200

    bit of work with new color palette

--- run.R
+++ run.R
@@ -5,11 +5,12 @@
 rm(list=ls(all=TRUE))
 gc()
 
+start_time <- Sys.time()
 #options(scipen=999) # disable scientific number formatting
 
 # set root directory
-# root.dir <- "~/btsync/faosync/pocketbooks/regional15/" # Markus
-root.dir <- "~/FAO/regional15/" # Amy
+root.dir <- "~/btsync/faosync/pocketbooks/regional15/" # Markus
+# root.dir <- "~/FAO/regional15/" # Amy
 
 setwd(root.dir)
 # set data directory
@@ -33,24 +34,27 @@ regionS_to_report <- c(
                       )
 ############################################################
 # Parts to include/exclude
-
+# -------------------------------
 include_timestamp    <- T
 include_foreword     <- T
 include_overview_map <- T
 include_overview_tbl <- F
+# -------------------------------
 include_part1        <- F
 include_part2        <- F
 include_part3        <- F
 include_part4        <- F
 include_part5        <- T
-include_part6        <- T
-include_country_profiles <- T
-include_definitions  <- T
-
+include_part6        <- F
+# -------------------------------
+include_country_profiles <- F
+include_definitions  <- F
+# -------------------------------
+# -------------------------------
 # Upgrade the comparison tables
 broke_all_into_images <- F
 broke_only_tables_into_images <- F
-
+# -------------------------------
 # To be uploaded for comments or not
 upload_pdfs_to_server <-   F
 upload_images_to_server <- F
