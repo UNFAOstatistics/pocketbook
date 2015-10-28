572867f5c070d780c83f05b00225ae6aa0f3a9d8
Markus Kainu <markus.kainu@fao.org>
2015-10-28 17:15:23 +0100

    maintenance

--- run.R
+++ run.R
@@ -52,8 +52,8 @@ include_part6        <- F
 # include_part9        <- F # just a placeholder
 # include_part10       <- F # just a placeholder
 # -------------------------------
-include_country_profiles <- T
-include_definitions      <- T
+include_country_profiles <- F
+include_definitions      <- F
 include_notes            <- F
 # -------------------------------
 # Upgrade the comparison tables
@@ -61,7 +61,7 @@ broke_all_into_images         <- F
 broke_only_tables_into_images <- F
 # -------------------------------
 # To be uploaded for comments or not
-upload_pdfs_to_server   <- F
+upload_pdfs_to_server   <- T
 upload_images_to_server <- F
 # just for troubleshooting
 region_to_report <- "RAF"
