3aa727bd42b6ed01c02f0c3f955edf89e3e4ed9b
Markus Kainu <markus.kainu@fao.org>
2015-10-30 17:46:35 +0100

    new covers

--- input/templates/cover/cropping_covers.sh
+++ input/templates/cover/cropping_covers.sh
@@ -1,6 +1,28 @@
+# RAP
+pdfcrop --margins '-331 -0 -0 -0' RAP_cover.pdf front_cover_RAP.pdf
+pdfcrop --margins '-0 -0 -331 -0' RAP_cover.pdf back_cover_RAP.pdf
+
+# RAF
+pdfcrop --margins '-331 -0 -0 -0' RAF_cover.pdf front_cover_RAF.pdf
+pdfcrop --margins '-0 -0 -331 -0' RAF_cover.pdf back_cover_RAF.pdf
+
+# REU
+pdfcrop --margins '-331 -0 -0 -0' REU_cover.pdf front_cover_REU.pdf
+pdfcrop --margins '-0 -0 -331 -0' REU_cover.pdf back_cover_REU.pdf
+
+# RNE
+pdfcrop --margins '-331 -0 -0 -0' RNE_cover.pdf front_cover_RNE.pdf
+pdfcrop --margins '-0 -0 -331 -0' RNE_cover.pdf back_cover_RNE.pdf
+
+
+
+# COF
 # New small one
 pdfcrop --margins '-332 -0 -0 -0' COVER_FaoPocket_2015_Cofee-rev1-light.pdf front_cover_COF.pdf
 pdfcrop --margins '-0 -0 -332 -0' COVER_FaoPocket_2015_Cofee-rev1-light.pdf back_cover_COF.pdf
+
+
+
 
 
 # File with crop marks (HUGE 52MB)
