99e8c0a0f353722486c6cec4b20daa716162c88a
Markus Kainu <markus.kainu@fao.org>
2015-11-18 18:49:34 +0100

    maintenance

--- run.R
+++ run.R
@@ -18,12 +18,12 @@ data.dir <- paste0(root.dir,"/input/data/database/")
 
 ## ---- chapters_to_include ----
 regionS_to_report <- c(
-                      "GLO" # Global
-                        ,"RAP" # Asia and the Pacific
-                        ,"RAF"  # Africa
-                        ,"REU" # Europe and Central Asia
-                        ,"RNE" # Near East and North Africa
-                        ,"COF" # Coffee
+                      # "GLO" # Global
+                        # "RAP" # Asia and the Pacific
+                        # ,"RAF"  # Africa
+                        "REU" # Europe and Central Asia
+                        # ,"RNE" # Near East and North Africa
+                        # ,"COF" # Coffee
                         #,"LAC" # Latin America and the Caribbean
                       )
 ############################################################
@@ -46,8 +46,8 @@ include_part1        <- T
 include_part2        <- T
 include_part3        <- T
 include_part4        <- T
-include_part5        <- T
-include_part6        <- T
+include_part5        <- F
+include_part6        <- F
 # include_part7        <- F # just a placeholder
 # include_part8        <- F # just a placeholder
 # include_part9        <- F # just a placeholder
@@ -62,7 +62,7 @@ broke_all_into_images         <- F
 broke_only_tables_into_images <- F
 # -------------------------------
 # To be uploaded for comments or not
-upload_pdfs_to_server   <- T
+upload_pdfs_to_server   <- F
 upload_images_to_server <- F
 # ------------------------------
 # for latex tables etc. latex specific stuff
@@ -186,7 +186,9 @@ FAOcountryProfile$SHORT_NAME[FAOcountryProfile$FAOST_CODE == 116] <- "Korea, Dem
 # load(paste0(data.dir,"/SYB2015-10-14.RData"))
 # load(paste0(data.dir,"/SYB2015-10-15.RData"))
 # load(paste0(data.dir,"/SYB2015-10-20.RData"))
-load(paste0(data.dir,"/SYB2015-11-05.RData"))
+# load(paste0(data.dir,"/SYB2015-11-05.RData"))
+# load("/home/markus/btsync/faosync/syb_database/output_data/2015-11-18_night/SYB2015-11-18.RData")
+load("/home/markus/btsync/faosync/syb_database/output_data/2015-11-18/SYB2015-11-18.RData")
 
 syb.df <- SYB.df; rm(SYB.df)
 
